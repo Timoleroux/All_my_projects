3eed6724965b535a057aae6ff1ba5f94085b5d88
Timothée Le Roux <timoleroux2006@gmail.com>
2021-07-17 13:31:46 +0200

    Create UI

---
+++ app.py
@@ -0,0 +1,36 @@
+from PySide2 import QtGui, QtWidgets
+from PySide2.QtCore import QWaitCondition, Qt
+import os
+import json
+
+PATH = "C:/Users/timol/OneDrive/Documents/GitHub/Note/data/data.json"
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Solids calculator")
+        self.setWindowIcon(QtGui.QIcon('C:/Users/timol/OneDrive/Documents/GitHub/SolidsCalculator/data/icon.ico'))
+        self.component()
+        self.css()
+
+    def component(self):
+        self.main_layout = QtWidgets.QHBoxLayout(self)
+        self.choice = QtWidgets.QComboBox()
+        self.save = QtWidgets.QPushButton("")
+        self.note_list = QtWidgets.QListWidget()
+
+        self.main_layout.addWidget(self.choice)
+        self.choice.addItem("Cube")
+        self.choice.addItem("Pavé droit")
+        self.choice.addItem("Cone")
+
+
+    def css(self):
+        self.setStyleSheet("""
+        background-color: #ffffff;
+        """)
+
+app = QtWidgets.QApplication([])
+win = App()
+win.show()
+app.exec_()
